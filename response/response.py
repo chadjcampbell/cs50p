028f1e0f5bae2a5e1a99eeee57bdeb646bad6ca1
Chad <chadjcampbell@gmail.com>
2023-11-17 19:59:57 -0500

    week 7 done

---
+++ response/response.py
@@ -0,0 +1,20 @@
+from validator_collection import validators
+
+
+def main():
+    print(validate(input("What's your email address? ")))
+
+
+def validate(s):
+    try:
+        validators.email(s)
+        return 'Valid'
+    except:
+        return 'Invalid'
+
+
+
+
+
+if __name__ == "__main__":
+    main()
